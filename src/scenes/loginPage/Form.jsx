c728143ffd6fd062b48fab4c5337e54e7e51af37
Mayuresh308 <mayureshmayuresh56@gmail.com>
2024-08-20 23:14:58 +0530

    commit

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -64,8 +64,8 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      // "http://localhost:3001/auth/register",
-      "https://social-media-backend-lxzd.onrender.com/auth/register",
+      "http://localhost:3001/auth/register",
+      // "https://social-media-backend-lxzd.onrender.com/auth/register",
 
       {
         method: "POST",
@@ -81,8 +81,8 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    // const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
-      const loggedInResponse = await fetch("https://social-media-backend-lxzd.onrender.com/auth/login", {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      // const loggedInResponse = await fetch("https://social-media-backend-lxzd.onrender.com/auth/login", {
 
       method: "POST",
       headers: { "Content-Type": "application/json" },
