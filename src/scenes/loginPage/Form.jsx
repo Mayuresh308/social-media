4a51ef3b0347517bb94742f6ad7f2aca59d425e5
Mayuresh308 <mayureshmayuresh56@gmail.com>
2024-08-20 23:17:45 +0530

    commit

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -81,8 +81,8 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
-      // const loggedInResponse = await fetch("https://social-media-backend-lxzd.onrender.com/auth/login", {
+    // const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      const loggedInResponse = await fetch("https://social-media-backend-lxzd.onrender.com/auth/login", {
 
       method: "POST",
       headers: { "Content-Type": "application/json" },
